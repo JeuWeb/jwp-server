de71d42474f2a63c9328c9408d3c9b34c9cb1260
Romain Tribes <tribes.romain@gmail.com>
2020-04-23 17:51:45 +0200

    Run server in production.

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :jwp, JwpWeb.Endpoint,
   url: [host: "localhost", port: 4000],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
